0887d2723e54d09e3ec7f1545f3ca077c5fd58e7
Eugen Guriev <gto200313@gmail.com>
2022-06-13 00:34:16 +0300

    added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { Telegram } from "../../assets/icons/Telegram";
+import { Badge } from "../Badge/Badge";
 import footer from "./Footer.module.scss";
 
 export const Footer = () => {
   return (
     <div className={footer.footer}>
-      <h2 className={footer.title}>contact</h2>
+      <div className={footer.footer__wrap}>
+        <div className={footer.left}>
+          <h2 className={footer.title}>contact</h2>
+
+          <div className={footer.iconRow}>
+            <Badge
+              type="footer"
+              icon={<Telegram />}
+              link="https://t.me/Loner_feed"
+            />
+          </div>
+
+          <div className={footer.info}>
+            <span className={footer.info__heading}>e-mail</span>
+
+            <span className={footer.info__sub}>gto200313@gmail.com</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
